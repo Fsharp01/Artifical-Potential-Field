9f65c6ca0e1eef0ce1a5052da9cc6966de296602
Losi <losonczidani2001@gmail.com>
2023-04-14 00:49:21 +0200

    Callback: Calculating Path

--- potential_field_pathplan.m
+++ potential_field_pathplan.m
@@ -164,3 +164,121 @@ function amclCallback(~, msg2)
 
    
 end
+function moveBaseGoalCallback(~, msg3)
+    global pub_path
+    global GoalpositionX
+    global GoalpositionY
+    global X
+    global Y
+    global x
+    global y
+    global xO
+global yO
+global obstacle
+
+global xO2
+global yO2
+global obstacle2
+
+
+%define the obstackle radius
+global r
+global r2
+global r3
+
+% Define the field spread
+global s
+global s2
+global s3
+
+% Define the constant
+global k
+global k2
+global k3
+global blankPoseMsg
+global blankPathMsg
+global position
+global robot_pos
+
+
+step_size=0.5;
+
+
+
+    % Access the position and orientation data from the message
+    GoalpositionX = msg3.Pose.Position.X/0.1;
+    GoalpositionY = msg3.Pose.Position.Y/0.1;
+    goal = [GoalpositionX, GoalpositionY];
+    disp(['Goal: (' num2str(GoalpositionX) ', ' num2str(GoalpositionY) ')']) 
+
+    
+
+    % tervezés
+    for i = 1:numel(X)
+
+   
+    % Calculate the action vector for the robot's current position
+    actionVector4 = calculateActionVector(robot_pos,GoalpositionX, GoalpositionY, r, s, k) ...
+        + calculateActionVector2(robot_pos,  xO, yO, r2, s2, k2) ...
+        + calculateActionVector3(robot_pos,  xO2, yO2, r3, s3, k3);
+
+    % Normalize the action vector to obtain a unit vector in the direction of motion
+    unit_vector = actionVector4 / norm(actionVector4);
+    
+    % Move the robot in the direction of the action vector at the fixed step size
+    new_pos = robot_pos + step_size .* unit_vector;
+
+      % Check if the robot has reached the goal position
+    if norm(new_pos - goal) < 0.01
+        disp("Robot reached goal!");
+        break;
+    end
+
+
+% Define the safety distance from the obstacles
+safety_dist = 0.5;
+
+% Check if the robot collides with an obstacle
+if norm(new_pos - obstacle) < r2 + safety_dist
+    % Compute the vector from the obstacle to the robot
+    vector_to_robot = robot_pos - obstacle;
+    % Compute the tangent vector to the obstacle
+    tangent_vector = [-vector_to_robot(2); vector_to_robot(1)];
+    % Compute the component of the action vector that's parallel to the tangent vector
+    parallel_vector = dot(unit_vector, tangent_vector) * tangent_vector;
+    % Compute the component of the action vector that's orthogonal to the tangent vector
+    orthogonal_vector = unit_vector - parallel_vector;
+    % Compute the new action vector by reversing the orthogonal vector and adding it to the parallel vector
+    new_unit_vector = parallel_vector - orthogonal_vector;
+    % Move the robot in the direction of the new action vector
+    robot_pos = robot_pos + step_size * new_unit_vector;
+elseif norm(new_pos - obstacle2) < r3 + safety_dist
+    % Compute the vector from the obstacle to the robot
+    vector_to_robot = robot_pos - obstacle2;
+    % Compute the tangent vector to the obstacle
+    tangent_vector = [-vector_to_robot(2); vector_to_robot(1)];
+    % Compute the component of the action vector that's parallel to the tangent vector
+    parallel_vector = dot(unit_vector, tangent_vector) * tangent_vector;
+    % Compute the component of the action vector that's orthogonal to the tangent vector
+    orthogonal_vector = unit_vector - parallel_vector;
+    % Compute the new action vector by reversing the orthogonal vector and adding it to the parallel vector
+    new_unit_vector = parallel_vector - orthogonal_vector;
+    % Move the robot in the direction of the new action vector
+    robot_pos = robot_pos + step_size * new_unit_vector;
+else
+    % Move the robot to the new position
+    robot_pos = new_pos;
+end
+
+       % Append the robot's x and y positions to the arrays
+    x = [x, robot_pos(1)];
+    y = [y, robot_pos(2)];
+  
+    end
+
+load blankPoseMsg;
+load blankPathMsg;
+%% Initilaize Path size
+[~,maxSize] = size(x);
+blankPoseMsgArray = repmat(blankPoseMsg,maxSize,1);
+blankPathMsg.Poses = blankPoseMsgArray;
