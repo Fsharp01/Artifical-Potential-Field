07936b391804111e65455141f984786c28239889
Losi <losonczidani2001@gmail.com>
2023-04-14 00:31:03 +0200

    Calculation of Goal attraction

---
+++ calculateActionVector.m
@@ -0,0 +1,13 @@
+function actionVector = calculateActionVector(position, GoalpositionX, GoalpositionY, r, s, k)
+    x = position(1);
+    y = position(2);
+    d = sqrt((x - GoalpositionX)^2 + (y - GoalpositionY)^2);
+    angle = atan2(GoalpositionY - y, GoalpositionX - x);
+    if d < r
+        actionVector = [0, 0];
+    elseif all([r <= d, d <= s + r])
+        actionVector = k * (d - r) * [cos(angle), sin(angle)];
+    else
+        actionVector = k * s * [cos(angle), sin(angle)];
+    end
+end
