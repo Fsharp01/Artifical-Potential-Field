13215fc0891ec3b95fb5180dfaac603a902035ca
Losi <losonczidani2001@gmail.com>
2023-04-14 00:52:32 +0200

    Missing Declaration

--- potential_field_pathplan.m
+++ potential_field_pathplan.m
@@ -2,26 +2,11 @@ masterhost='http://losi-pc:11311';
 rosinit(masterhost)
 
 %% Create blank Pose message
+global blankPoseMsg
+global blankPathMsg
+
 load ./MAT/msg/blankPoseMsg blankPoseMsg;
 load ./MAT/msg/blankPathMsg blankPathMsg;
-
-%% Initilaize Path size
-[~,maxSize] = size(x);
-blankPoseMsgArray = repmat(blankPoseMsg,maxSize,1);
-blankPathMsg.Poses = blankPoseMsgArray;
-
-%% Define a grid of points in the 2D space
-x = -15:0.5:15;
-y = -15:0.5:15;
-[X, Y] = meshgrid(x, y);
-
-
-sub2 = rossubscriber('agent1/pose/amcl', 'geometry_msgs/PoseWithCovarianceStamped', @amclCallback);
-sub3 = rossubscriber('/move_base_simple/goal', 'geometry_msgs/PoseStamped', @moveBaseGoalCallback);
-pub_path = rospublisher("path","nav_msgs/Path", "DataFormat","struct");
-pathMsg = blankPathMsg;
-
-
 %% global Variables
 global x
 global y
@@ -62,6 +47,24 @@ global position
 
 global blankPoseMsg
 global blankPathMsg
+
+
+%% Initilaize Path size
+[~,maxSize] = size(x);
+blankPoseMsgArray = repmat(blankPoseMsg,maxSize,1);
+blankPathMsg.Poses = blankPoseMsgArray;
+
+%% Define a grid of points in the 2D space
+x = -15:0.5:15;
+y = -15:0.5:15;
+[X, Y] = meshgrid(x, y);
+
+
+sub2 = rossubscriber('agent1/pose/amcl', 'geometry_msgs/PoseWithCovarianceStamped', @amclCallback);
+sub3 = rossubscriber('/move_base_simple/goal', 'geometry_msgs/PoseStamped', @moveBaseGoalCallback);
+pub_path = rospublisher("path","nav_msgs/Path", "DataFormat","struct");
+pathMsg = blankPathMsg;
+
 
 
 
